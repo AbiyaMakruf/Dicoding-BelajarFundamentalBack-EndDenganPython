8014b0365f41b6bb8933e6408c334b9f1d56553a
AbiyaMakruf <aabbiiyyaa@gmail.com>
2025-09-20 15:16:48 +0700

    add: menyelesaikan groups

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions, response
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.models import Group
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,3 +17,26 @@ class UserViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return response.Response({"users": serializer.data})
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return response.Response({"groups": serializer.data})
+
+class AssignRoleView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        group_id = request.data.get('group_id')
+
+        user = get_object_or_404(User, id=user_id)
+        group = get_object_or_404(Group, id=group_id)
+
+        user.groups.add(group)
+        return Response({"message": f"User {user.username} assigned to group {group.name}"}, status=status.HTTP_201_CREATED)
